c5c77c8ba67b56eb9aa23b1e57daffa1e0c1deed
Erion Pici <erionpc@gmail.com>
2018-11-01 01:29:41 +0000

    Add project files.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(string category, int productPage = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .Where(p => matchCategory(p, category))
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count(p => matchCategory(p, category))
+                },
+                CurrentCategory = category
+            });
+
+        private bool matchCategory(Product prod, string category) 
+            => category == null || prod.Category == category;
+    }
+}
